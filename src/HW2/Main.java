5ee9d210e01f631b5be9ad414102c7dd95138f14
Kirill Poliak <poliak.kirill@gmail.com>
2023-10-23 21:21:32 +0300

    //Homework 2

---
+++ src/HW2/Main.java
@@ -0,0 +1,6 @@
+package HW2;
+public class Main {
+    public static void main(String[] args) {
+        Employee Worker = new Employee("John", "Operator", "john.operator@gmail.com", "+380485618243", 28);
+    }
+}
